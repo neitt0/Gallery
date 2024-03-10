d169575211811f6bf6d6eabda116927f221c77de
Thien Nguyen <114895619+neitt0@users.noreply.github.com>
2024-03-10 04:45:22 -0400

    add images straight to html and fix toggle issue

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Gallery Website
 Inspired by nathanmartell.com
+Nothing is copied except design
+Project is meant for learning while building
+
+## MVP
+Looks like reference page
+All/most buttons function as expected
+Smooth transition/hover effects
--- index.js
+++ index.js
@@ -2,34 +2,38 @@ let buttons = document.querySelectorAll('button')
 let indexBtn = document.querySelector('.index')
 let infoBtn = document.querySelector('.info')
 
+let indexContent = document.querySelector('.index-content')
+let infoContent = document.querySelector('.info-content')
+
 for (b of buttons) {
-  // b.addEventListener('click', () => {
+  b.addEventListener('click', () => {
     // stop main page from scrolling while nav in open
     let body = document.querySelector('body')
     let nav = document.querySelector('nav')
-    body.style.overflow = 'hidden';
-    nav.style.overflow = 'scroll'
-    // target button's grandparent
+
+    
+    body.classList.toggle('scroll-toggle')
+    nav.classList.toggle('scroll-toggle')
+    
+    // target button's grandparent ()
     b.parentElement.parentElement.classList.toggle('fullNav')
-  // })
+  })
 }
 
-// indexBtn.addEventListener('click', () => {
-  let articleCells = document.querySelectorAll('.article-content')
-  let navContent = document.querySelector('.nav-content')
-
-  for (cell of articleCells) {
-    if (cell.children.length > 0) {
-      let tempContent = document.createElement('div')
-
-      tempContent.appendChild(cell.children[0])
-      tempContent.classList.add('indexImgContainer') 
-
-      navContent.appendChild(tempContent)
-    }
+indexBtn.addEventListener('click', () => {
+  if (b.parentElement.parentElement.classList.contains('fullNav')) {
+    indexContent.hidden = false;
+  } else {
+    indexContent.hidden = true;
+    infoContent.hidden = true;
   }
-// })
+})
 
 infoBtn.addEventListener('click', () => {
-
+  if (b.parentElement.parentElement.classList.contains('fullNav')) {
+    infoContent.hidden = false;
+  } else {
+    indexContent.hidden = true;
+    infoContent.hidden = true;
+  }
 })
